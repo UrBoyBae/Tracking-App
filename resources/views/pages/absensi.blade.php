9896151c7c4ccce36ab4205ea5421475375b6ec0
Ali Akbar Abdillah <aliakbarabdillah21@gmail.com>
2023-02-23 12:09:16 +0700

    ubah title

--- resources/views/pages/absensi.blade.php
+++ resources/views/pages/absensi.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{asset('assets/images/v-tax-logo.png')}}" rel="shortcut icon">
-    <title>Attendance | Admin</title>
+    <title>Testing</title>
     <!-- My CSS -->
     <link rel="stylesheet" href="{{asset('assets/css/absensiStyle.css')}}">
     <!-- Google Font -->
